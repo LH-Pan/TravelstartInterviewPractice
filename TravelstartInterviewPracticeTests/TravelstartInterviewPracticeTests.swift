bee62a7c96d588aab7f6cf00e4d07a9812de91c1
潘立祥 <salt918077@gmail.com>
2019-11-02 11:43:14 +0800

    [Add]: 建立檔案及 pod file

---
+++ TravelstartInterviewPracticeTests/TravelstartInterviewPracticeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TravelstartInterviewPracticeTests.swift
+//  TravelstartInterviewPracticeTests
+//
+//  Created by 潘立祥 on 2019/11/1.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import XCTest
+@testable import TravelstartInterviewPractice
+
+class TravelstartInterviewPracticeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
