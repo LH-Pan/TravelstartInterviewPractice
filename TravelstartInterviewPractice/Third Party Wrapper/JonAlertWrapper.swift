a6d6d336d29e3e52c6ff91a3a51d51377f4ba61a
潘立祥 <salt918077@gmail.com>
2019-11-03 21:41:57 +0800

    [Add]: 建立第三方套件 JonAlert Wrapper

---
+++ TravelstartInterviewPractice/Third Party Wrapper/JonAlertWrapper.swift
@@ -0,0 +1,32 @@
+//
+//  JonAlertWrapper.swift
+//  TravelstartInterviewPractice
+//
+//  Created by 潘立祥 on 2019/11/3.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import JonAlert
+
+class TIPJonAlert {
+        
+    static func showSuccess(message: String) {
+        
+        JonAlert.showSuccess(message: message)
+    }
+    
+    static func showError(message: String) {
+        
+        JonAlert.showError(message: message)
+    }
+    
+    static func showMessage(message: String) {
+        
+        JonAlert.show(message: message)
+    }
+    
+    static func showCustomIcon(icon image: UIImage?, message: String) {
+        
+        JonAlert.show(message: message, andIcon: image)
+    }
+}
